d6cdf85deae345629085b0da0383e5c9a10c668e
mikejigglypuff <k24555569@gmail.com>
2024-12-31 16:35:01 +0900

    🔧: (level 2-2) 처리할 예외 추가

--- src/main/java/com/calendarManagerV2/advanced_lv1and2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/calendarManagerV2/advanced_lv1and2/exception/GlobalExceptionHandler.java
@@ -3,16 +3,23 @@ package com.calendarManagerV2.advanced_lv1and2.exception;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 // 프로젝트 전역에서 발생한 예외들에 대해 알맞은 Response Status 및 메시지를 붙여 응답하도록 함
 @ControllerAdvice
@@ -24,10 +31,13 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({
         NoSuchElementException.class,
+        NoResourceFoundException.class,
         IllegalArgumentException.class,
         ValidationFailedException.class,
+        MissingPathVariableException.class,
         ConstraintViolationException.class,
         DataIntegrityViolationException.class,
+        InvalidDataAccessApiUsageException.class,
         MethodArgumentTypeMismatchException.class
     })
     public ResponseEntity<String> handleBadRequests(Exception e) {
@@ -45,9 +55,22 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<String> handleHandlerMethodValidationException(HandlerMethodValidationException e) {
+        String messages = Arrays.stream(e.getDetailMessageArguments())
+            .map(Object::toString)
+            .collect(Collectors.joining("\n"));
+        return makeStringResBody(HttpStatus.BAD_REQUEST, messages);
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
         return makeStringResBody(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+    @ExceptionHandler(IncorrectResultSizeDataAccessException.class)
+    public ResponseEntity<String> handleIncorrectResultSizeDataAccessException(IncorrectResultSizeDataAccessException e) {
+        return makeStringResBody(HttpStatus.CONFLICT, "조회 결과가 예상보다 많습니다.");
     }
 
     // 반복되는 ResponseEntity 설정 코드 간소화
